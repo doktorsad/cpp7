e94e3a0c9607d59e6f50291b530ca63aa3609fa2
cQula <doktorsad@yandex.ru>
2022-12-06 16:31:12 +0300

    Initial commit

---
+++ cpp7_1/cpp7_1/cpp7_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#define MODE 0
+#if !defined MODE
+#error MODE not defined
+#endif
+
+int add(const int& x, const int& y) {
+	return x + y;
+}
+
+int main()
+{
+#if MODE == 0
+	std::cout << "I work in training mode\n";
+#elif MODE == 1
+	int x = 0, y = 0;
+	std::cout << "I work in combat mode\n" <<
+		"Enter two digits\n=> ";
+	std::cin >> x;
+	std::cout << "=> ";
+	std::cin >> y;
+	std::cout << "result = " << add(x, y);
+#else 
+	std::cout << "Unknown mode. Completion of work\n";
+#endif
+	return 0;
+}
---
+++ cpp7_2/cpp7_2/cpp7_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#define SUB(x,y) ((x) - (y))
+
+
+
+int main()
+{
+    int x = 20;
+    int y = 5;
+    int c = 2;
+    std::cout << SUB(x, y) <<'\n';
+    std::cout << SUB(x, y) * c <<'\n';
+    std::cout << SUB(x, y + c) * c << '\n';
+    return 0;
+}
+
